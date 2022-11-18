cff1ed695db78287c18a3ec93e7ef920a03e4813
Jakub Gluch <jakub.gluch92@gmail.com>
2022-11-18 11:34:56 +0100

    index.js code formating

--- src/Main/index.js
+++ src/Main/index.js
@@ -1,13 +1,12 @@
 import "./style.css"
 
-const Main = ({title, body}) => (
+const Main = ({ title, body }) => (
     <section>
         <h2 className="main__header">
             {title}
         </h2>
         {body}
     </section>
-    
-    );
+);
 
-    export default Main;
+export default Main;
