81ad30dd818cbfad2e1656e991b30ca4ec917da4
Jakub Gluch <jakub.gluch92@gmail.com>
2022-11-18 11:42:58 +0100

    Section/index.js code formating

--- src/Section/index.js
+++ src/Section/index.js
@@ -1,17 +1,17 @@
 import "./style.css"
 
 const Section = ({ title, body, extraHeaderContent }) => (
-    <section>
-        <div className="taskList__section">
-            <h2 className="taskList__header">
-                {title}
-            </h2>
-            <div className="taskList__buttons" >
-                {extraHeaderContent}
-            </div>
-        </div>
-        {body}
-    </section>
-);
+  <section>
+    <div className="taskList__section">
+      <h2 className="taskList__header">
+        {title}
+      </h2>
+      <div className="taskList__buttons" >
+        {extraHeaderContent}
+      </div>
+    </div>
+    {body}
+  </section>
+)
 
 export default Section;
