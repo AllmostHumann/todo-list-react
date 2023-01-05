726302e753dcc6d770add9bc51e5829579bcfe0c
Jakub Gluch <jakub.gluch92@gmail.com>
2023-01-05 18:22:19 +0100

    Container css changed to styled-components

--- src/Container/styled.js
+++ src/Container/styled.js
@@ -1,8 +1,10 @@
-.container {
+import styled from "styled-components";
+
+export const MainContainer = styled.main`
     margin: 0 auto;
     text-align: left;
     max-width: 1000px;
     padding: 20px;
     line-height: 1.5;
     background-color: rgb(218, 218, 218);
-}
+`;
