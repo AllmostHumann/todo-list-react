7e49604966549a704a21b9b909541b370290f2f5
Jakub Gluch <jakub.gluch92@gmail.com>
2023-01-11 16:04:20 +0100

    styled components moved to Container index.js

--- src/App/Container/index.js
+++ src/App/Container/index.js
@@ -1,9 +1,12 @@
-import { MainContainer } from "./styled";
+import styled from "styled-components";
 
-const Container = ({ children }) => (
-    <MainContainer>
-        {children}
-    </MainContainer>
-);
+const Container = styled.main`
+    margin: 0 auto;
+    text-align: left;
+    max-width: 1000px;
+    padding: 20px;
+    line-height: 1.5;
+    background-color: rgb(218, 218, 218);
+`;
 
 export default Container;
