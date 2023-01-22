e34f3395fad7c7f344c7a75f8872be4df4378f04
Jakub Gluch <jakub.gluch92@gmail.com>
2023-01-22 15:57:23 +0100

    Created App.js to replace Tasks in main index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Tasks from './features/tasks/Tasks.js';
+import { App } from "./App";
 import reportWebVitals from './reportWebVitals';
 import GlobalStyle from './globalStyle';
 
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <GlobalStyle />
-    <Tasks />
+    <App />
   </React.StrictMode>
 );
 
