b20e3d62a76ca87250334f301fbbf9691645d3c4
Jakub Gluch <jakub.gluch92@gmail.com>
2023-01-30 19:44:34 +0100

    styled components for new button

--- src/common/Section/index.js
+++ src/common/Section/index.js
@@ -1,19 +1,31 @@
-import { Body, Container, List } from "./styled";
+import { Body, Container, List, TopSection } from "./styled";
+import { ExampleButton } from "./styled";
+import { useDispatch } from "react-redux";
+import { fetchExampleTasks } from "../../features/tasks/tasksSlice";
 
-const Section = ({ listTitle, listContent, bodyTitle, bodyContent, buttons }) => (
-  <section>
-    <Body>
-      {bodyTitle}
-    </Body>
-    {bodyContent}
-    <Container>
-      <List>
-        {listTitle}
-      </List>
-      {buttons}
-    </Container>
-    {listContent}
-  </section>
-);
+const Section = ({ listTitle, listContent, bodyTitle, bodyContent, buttons }) => {
+  const dispatch = useDispatch();
+
+  return (
+    <section>
+      <TopSection>
+        <Body>
+          {bodyTitle}
+        </Body>
+        <ExampleButton onClick={() => dispatch(fetchExampleTasks())}>
+          Pobierz przykładowe zadania
+        </ExampleButton>
+      </TopSection>
+      {bodyContent}
+      <Container>
+        <List>
+          {listTitle}
+        </List>
+        {buttons}
+      </Container>
+      {listContent}
+    </section>
+  )
+};
 
 export default Section;
--- src/common/Section/styled.js
+++ src/common/Section/styled.js
@@ -1,5 +1,19 @@
 import styled from "styled-components";
 
+export const TopSection = styled.div`
+    border-bottom: 5px solid hsl(0, 0%, 87%);
+    background-color: hsl(0, 0%, 100%);
+    display: grid;
+    grid-template-columns: auto auto;
+    justify-content: space-between;
+    align-items: center;
+    gap: 10px;
+    padding: 0 10px;
+@media (max-width: 767px) {
+        grid-template-columns: 1fr;
+    }
+`;
+
 export const Container = styled.div`
     border-bottom: 1px solid hsl(0, 0%, 87%);
     background-color: hsl(0, 0%, 100%);
@@ -18,9 +32,9 @@ export const List = styled.h2`
     font-size: 25px;
     font-weight: 700;
     margin: auto;
+    padding: 0 10px 0 10px;
 @media (max-width: 767px) {
         margin: 0;
-        background-color: hsl(0, 0%, 100%);
         font-weight: 700;
         font-size: 20px;
 }
@@ -28,15 +42,31 @@ export const List = styled.h2`
 
 export const Body = styled.h2`
     padding: 10px;
-    margin: 5px auto;
+    margin: auto;
     background-color: hsl(0, 0%, 100%);
     font-weight: 700;
     font-size: 25px;
 @media (max-width: 767px) {
-        padding: 10px;
-        margin: 5px auto;
-        background-color: hsl(0, 0%, 100%);
+        margin: 0;
         font-weight: 700;
         font-size: 20px;
+}
+`;
+
+export const ExampleButton = styled.button`
+    border: none;
+    background-color: hsl(0, 0%, 100%);
+    color: teal;
+    margin: 0;
+    transition: color 0.5s;
+@media (max-width: 767px) {
+        margin: 10px;
+};
+
+&:hover {
+    color: hsla(180, 100%, 25%, 0.822);
+}
+&:disabled {
+    color: hsl(0, 0%, 80%);
 }
 `;
