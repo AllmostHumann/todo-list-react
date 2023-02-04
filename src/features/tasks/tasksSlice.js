7b3a4a68911c12a179d216795162c7a1fe96b1e0
Jakub Gluch <jakub.gluch92@gmail.com>
2023-02-04 13:58:05 +0100

    typo correct

--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -60,7 +60,7 @@ export const {
 export const selectTasksState = (state) => state.tasks;
 export const selectTasks = (state) => selectTasksState(state).tasks;
 export const selectHideDone = (state) => selectTasksState(state).hideDone;
-export const selectAreTasksEmpty = (state) => selectTasks(state).length === 0;
+export const selectAreTasksEmpty = (state) => selectTasks(state).lenght === 0;
 export const selectIsEveryTaskDone = (state) => selectTasks(state).every(({ done }) => done);
 export const selectLoading = (state) => selectTasksState(state).loading;
 export const getTaskById = (state, taskId) => selectTasks(state).find(({ id }) => id === taskId);
