3516f7077c6f5473ab8e8c1d1a0efe9155f57545
Jakub Gluch <jakub.gluch92@gmail.com>
2023-02-04 14:51:46 +0100

    axios change

--- src/features/tasks/TasksPage/TasksList/getExapleTasks.js
+++ src/features/tasks/TasksPage/TasksList/getExapleTasks.js
@@ -2,8 +2,6 @@ import axios from "axios";
 
 export const getExampleTasks = async () => {
   const response = await axios.get("/todo-list-react/exampleTasks.json");
-  if (response.statusText !== "OK") {
-    new Error(response.statusText);
-  }
+
   return response.data;
 };
